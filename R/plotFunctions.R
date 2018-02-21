4077f3cae11af209cbc5fbe4d75af7ed042482ce
Jie Ping <pingjie811@gmail.com>
2018-02-21 14:40:24 -0600

    rewrite plot_multi_dens with ggplot2 density function

--- R/plotFunctions.R
+++ R/plotFunctions.R
@@ -1,4 +1,4 @@
-
+library(ggplot2)
 
 checkClusterSeparateness <- function(object) {
   ####check the separateness of clusters using the silhouette width
@@ -13,7 +13,7 @@ checkClusterSeparateness <- function(object) {
   plot(sil, main = paste(length(unique(object$cluster)), "clusters "), border = sil.cols, col = sil.cols, do.col.sort = FALSE)
 }
 
-plot_multi_dens <- function(s, feature = "") {
+plot.multi.dens <- function(s, feature = "") {
   junk.x = NULL
   junk.y = NULL
   
@@ -33,7 +33,26 @@ plot_multi_dens <- function(s, feature = "") {
   }
 }
 
-plotMultiSamplesOneExplanatoryVariables <- function(s, var = "") {
+plot_multi_dens <- function(s, feature = "", cex = 2) {
+
+  fNo <- which(colnames(colData(s[[1]]$sce)) == feature)
+  
+  stopifnot(fNo > 0)
+  
+  pdata <- data.frame()
+  
+  for (i in 1:length(s)) {
+    pdata <- rbind(pdata, data.frame(data = colData(s[[i]]$sce)[, fNo], 
+                                     Sample = rep(names(s)[i], length((colData(s[[i]]$sce)[, fNo])))))
+  }
+  
+  p <- ggplot(pdata, aes(x = data, color = Sample)) + 
+    geom_density(cex = cex) + xlab(feature) + scale_color_manual(values = 1:length(s))
+  
+  return(p)
+}
+
+plotMultiSamplesOneExplanatoryVariables <- function(s, var = "", size = 2) {
   d <- list()
   pct_var_explained <- c()
   
@@ -46,11 +65,11 @@ plotMultiSamplesOneExplanatoryVariables <- function(s, var = "") {
                     Sample = rep(names(s), each = length(d[[1]]$data$Pct_Var_Explained)))
   
   p <- ggplot(dat, aes(x = Pct_Var_Explained, colour = Sample)) + 
-    geom_line(stat = "density", alpha = 0.7, size = 2, trim = T) + 
+    geom_line(stat = "density", alpha = 0.7, size = size, trim = T) + 
     geom_vline(xintercept = 1, linetype = 2) + 
     scale_x_log10(breaks = 10 ^ (-3:2), labels = c(0.001, 0.01, 0.1, 1, 10, 100)) + 
     xlab(paste0("% variance explained (log10-scale)")) + 
-    ylab("Density") + ggtitle(var) +
+    ylab("Density") + ggtitle(var) + scale_color_manual(values = 1:length(s)) +
     coord_cartesian(xlim = c(10 ^ (-3), 100)) 
   return(p)
 }
