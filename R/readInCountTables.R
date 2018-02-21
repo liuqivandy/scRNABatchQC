d55a820bd5dd94b3e0551587ea221f6b2d4a5acb
Jie Ping <pingjie811@gmail.com>
2018-02-21 13:52:17 -0600

    remove read in count table function

--- R/readInCountTables.R
+++ R/readInCountTables.R
@@ -1,18 +1,15 @@
 source("makeSCRNAdata.R")
 
-readInCountTables <- function(path, fileNames, sampleNames) {
-  #path = YOUR_FILE_PATH, 
-  #filenames = c("count1.csv","count2.csv", "count3.csv"),
-  #sampleNames = c("S1","S2", "S3")
-  
-  stopifnot(length(fileNames) == length(sampleNames))
-  
-  dat <- list()
-  
-  for (n in 1:length(filenames)) {
-    dat[[n]] <- makeSCRNAdata(fread(file.path(path, fileNames[n])))
-  }
-  names(dat) <- sampleNames
+path <- "D:/Work/scRNABatchQC" 
+fileNames <- c("count1.csv", "count2.csv", "count3.csv")
+sampleNames <- c("S1", "S2", "S3")
 
-  return(dat)
+dat <- list()
+  
+for (n in 1:length(fileNames)) {
+  counttable <- as.matrix(read.csv(file.path(path, fileNames[n]), row.names = 1))
+
+  dat[[n]] <- makeSCRNAdata(counttable)
 }
+names(dat) <- sampleNames
+
