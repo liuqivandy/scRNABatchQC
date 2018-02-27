2fc5c1e7501de7854ee4827f6c2e3bee0dfae8b4
Jie Ping <pingjie811@gmail.com>
2018-02-27 10:51:09 -0600

    add one more sample for testing purpose

--- R/run_scRNABatchQC.R
+++ R/run_scRNABatchQC.R
@@ -7,15 +7,16 @@ counts <- list()
 counts[[1]] <- as.matrix(t(read.csv(file.path(path, "qi_m2.csv"))))
 counts[[2]] <- as.matrix(t(read.csv(file.path(path, "s1_pan_qi_1.csv"))))
 counts[[3]] <- as.matrix(t(read.csv(file.path(path, "qi_m1.csv"))))
+counts[[4]] <- as.matrix(t(read.csv(file.path(path, "qi_m1.csv"))))
 genename <- read.csv(file.path(path, "mousenames.csv"), as.is = T, header = F)[, 1]
-rownames(counts[[1]]) <- rownames(counts[[2]]) <- rownames(counts[[3]]) <- genename
+rownames(counts[[1]]) <- rownames(counts[[2]]) <- rownames(counts[[3]]) <- rownames(counts[[4]]) <- genename
 
 sces <- list()
 
 for (n in 1 : length(counts)) {
   sces[[n]] <- prepareSCRNAData(counts[[n]])
 }
-names(sces) <- c("S1", "S2", "S3")
+names(sces) <- c("S1", "S2", "S3", "S3_2")
 
 sceall <- preparePCATSNEData(sces)
 
