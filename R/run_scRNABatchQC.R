f418f431eac0297da73e45549cb7fe3493366a3a
Jie Ping <pingjie811@gmail.com>
2018-02-27 11:12:57 -0600

    Update run_scRNABatchQC.R

--- R/run_scRNABatchQC.R
+++ R/run_scRNABatchQC.R
@@ -2,7 +2,7 @@ source("R/prepareSCRNAData.R")
 source("R/prepareSCRNADataSet.R")
 source("R/plotFunctions.R")
 
-sampleTable<-data.frame(Sample = c("S1", "S2", "S3"),
+sampleTable <- data.frame(Sample = c("S1", "S2", "S3"),
                         File = file.path("Z:/JiePing/scRNABatchQC", c("count1.csv", "count2.csv", "count3.csv")),
                         Transform = c(0, 0, 0))
 sces <- prepareSCRNADataSet(sampleTable)
