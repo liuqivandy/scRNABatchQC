648ff581b6846d7969b82d1aa6bc3f0a2c914690
Quanhu Sheng <shengqh@gmail.com>
2018-03-23 11:26:33 -0500

    output info

--- R/prepareReportData.R
+++ R/prepareReportData.R
@@ -1,35 +1,39 @@
 prepareReportData<-function(sampleTable, organism, cachePrefix){
+  cat("Preparing sample statistics data ...\n")
   if(!missing(cachePrefix)){
     scesFile = paste0(cachePrefix, "_sces.rdata")
     if(file.exists(scesFile)){
+      cat("Loading from cache file", scesFile, " ...\n")
       load(scesFile)
     }
     else{
       sces <- prepareSCRNADataSet(sampleTable, organism)
       save(sces, file=scesFile)
-      
     }
   }else{
     sces <- prepareSCRNADataSet(sampleTable, organism)
   }
   
+  cat("Preparing PCA and TSNe data ...\n")
   if(!missing(cachePrefix)){
     scesAllFile = paste0(cachePrefix, "_scesall.rdata")
     if(file.exists(scesAllFile)){
+      cat("Loading from cache file", scesAllFile, " ...\n")
       load(scesAllFile)
     }
     else{
       scesall <- preparePCATSNEData(sces)
       save(scesall, file=scesAllFile)
-      
     }
   }else{
     scesall <- preparePCATSNEData(sces)
   }
-
+  
+  cat("Preparing differential expression analysis data ...\n")
   if(!missing(cachePrefix)){
     diffFCFile = paste0(cachePrefix, "_diffFC.rdata")
     if(file.exists(diffFCFile)){
+      cat("Loading from cache file", diffFCFile, " ...\n")
       load(diffFCFile)
     }
     else{
@@ -43,5 +47,7 @@ prepareReportData<-function(sampleTable, organism, cachePrefix){
   hvgPathways<-.getMultiplePathway(sces, metaObjectName="hvgPathway")
   pc1Pathways<-.getMultiplePathway(sces, metaObjectName="pc1Pathway")
   plotData<-list(sces=sces, scesall=scesall, diffFC=diffFC, hvgPathways=hvgPathways, pc1Pathways=pc1Pathways)
+  
+  cat("Report data prepared.\n")
   return(plotData)
 }
