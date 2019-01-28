2effcb3837419a181553a8ff1b64b4a92168d9c5
liuqivandy <34580617+liuqivandy@users.noreply.github.com>
2019-01-28 12:40:36 -0600

    Update scRNABatchQC.R

--- R/scRNABatchQC.R
+++ R/scRNABatchQC.R
@@ -1,9 +1,9 @@
 scRNABatchQC<-function(inputfiles,samplenames=NULL, organism=c("hsapiens","mmusculus"),outputFile="report.html", sampleRatio=1,cache=FALSE, nHVGs=1000,nPC=10,sf=10000,logFC=1,FDR=0.01){
   organism<-match.arg(organism)
   if(!missing(cache) & cache){
-    plotData<-prepareReportData(inputfiles=inputfiles,samplenames=samplenames, organism=organism, outputFile=outputFile, sampleRatio=sampleRatio,nHVGs=nHVGs, nPC=nPC, sf=sf, logFC=logFC, FDR=FDR)
+    plotData<-prepareReportData(inputfiles=inputfiles,samplenames=samplenames, organism=organism,  sampleRatio=sampleRatio,nHVGs=nHVGs, nPC=nPC, sf=sf, logFC=logFC, FDR=FDR)
   }else{
-    plotData<-prepareReportData(inputfiles=inputfiles,samplenames=samplenames, organism=organism, outputFile=outputFile, sampleRatio=sampleRatio,nHVGs=nHVGs, nPC=nPC, sf=sf, logFC=logFC, FDR=FDR)
+    plotData<-prepareReportData(inputfiles=inputfiles,samplenames=samplenames, organism=organism,  sampleRatio=sampleRatio,nHVGs=nHVGs, nPC=nPC, sf=sf, logFC=logFC, FDR=FDR)
   }
   
   #reportRmd <- "E:/sqh/programs/scRNABatchQC/inst/report/scRNABatchQCreport.Rmd"
